10d1c18e0be07937ae0a4e93d3858fb8a6487e73
Aaron Feng <aaron.feng@gmail.com>
2011-05-11 21:34:37 -0400

    first commit

---
+++ amiworkingformashionnow.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+require 'net/https'
+
+mime_type :ttf, "application/octet-stream"
+mime_type :woff, "application/octet-stream"
+
+get '/' do
+  erb :index
+end
+
+post '/working' do
+  req = Net::HTTP::Get.new("/api/v2/json/user/show/#{params[:handle].downcase}/organizations")
+
+  http = Net::HTTP.new("github.com", 443)
+  http.use_ssl = true
+
+  myorgs = JSON.parse(http.request(req).body)["organizations"]
+
+  if !myorgs.select {|org| org["name"].eql?("Mashion")}.empty?
+    erb :yes
+  else
+    erb :no
+  end
+end
+
+get '/yes' do
+  erb :yes
+end
+
+get '/no' do
+  erb :no
+end
+
